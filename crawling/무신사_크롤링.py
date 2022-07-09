49c180167fd2f7f414686c4ae8e1a2821d42f9ab
HaJunYoo <hajuny129@gmail.com>
2022-07-09 19:32:54 +0900

    2nd commit

--- crawling/무신사_크롤링.py
+++ crawling/무신사_크롤링.py
@@ -79,8 +79,6 @@ def season_processing(data, season_find):
 ################################
 
 
-
-
 def crawling(num_li):
     # 크롬 드라이버 객체 생성
     driver = webdriver.Chrome("/Users/yoohajun/Library/Mobile Documents/com~apple~CloudDocs/Hajun/프로젝트/Cordi_RecSys/crawling/chromedriver")
@@ -95,78 +93,83 @@ def crawling(num_li):
 
         # 상품 코드 바꿔가면서 페이지 순회 => 상품 조회
         url = f'https://www.musinsa.com/app/goods/{num}'
-        driver.get(url)
 
-        # 1초에서 2초 사이 랜덤하게 휴식
-        driver.implicitly_wait(random.randint(1, 2))
+        try :
+            driver.get(url)
 
-        # 바뀐 url 정보를 parsing해서 soup변수에 저장
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
+            # 1초에서 2초 사이 랜덤하게 휴식
+            driver.implicitly_wait(random.randint(1, 2))
 
-        # 상품 이름
-        name = soup.select("div > div > span > em")[0].text
-        # 대분류
-        main_category = soup.select("div > div > p.item_categories > a")[0].text
-        # 중분류
-        sub_category = soup.select("div > div > p.item_categories > a")[1].text
-        # 브랜드
-        brand = soup.select("div > ul > li > p > strong > a")[0].text
-        # 품번
-        number = soup.select_one("div > ul > li > p > strong").text.split(" / ")[1]
-        # 해시태그
-        tags = [re.sub('[#]', '', s.text) for s in soup.select("li > p > a.listItem")]
-        # 가격
-        price = re.sub(" +|\n", "", soup.select("#goods_price")[0].text)
-        # 평점
-        rating = soup.select("span.prd-score__rating")[0].text
-        # 후기 개수
-        rating_num = soup.select("span.prd-score__review-count")[0].text
-        rating_num = re.sub(r'[^0-9]', '', rating_num)
-        # 시즌
-        season_word = soup.select("a.ui-toggle-btn")[0].text.split(" ")[0]
-        season_find = soup.select("li > p.product_article_contents > strong")[1].text
-        season = season_processing(season_word, season_find)
+            # 바뀐 url 정보를 parsing해서 soup변수에 저장
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-        # 성별
-        gender = [g.text for g in soup.select("li > p.product_article_contents > span.txt_gender > span")]
+            # 상품 이름
+            name = soup.select("div > div > span > em")[0].text
+            # 대분류
+            main_category = soup.select("div > div > p.item_categories > a")[0].text
+            # 중분류
+            sub_category = soup.select("div > div > p.item_categories > a")[1].text
+            # 브랜드
+            brand = soup.select("div > ul > li > p > strong > a")[0].text
+            # 품번
+            number = soup.select_one("div > ul > li > p > strong").text.split(" / ")[1]
+            # 해시태그
+            tags = [re.sub('[#]', '', s.text) for s in soup.select("li > p > a.listItem")]
+            # 가격
+            price = re.sub(" +|\n", "", soup.select("#goods_price")[0].text)
+            # 평점
+            rating = soup.select("span.prd-score__rating")[0].text
+            # 후기 개수
+            rating_num = soup.select("span.prd-score__review-count")[0].text
+            rating_num = re.sub(r'[^0-9]', '', rating_num)
+            # 시즌
+            season_word = soup.select("a.ui-toggle-btn")[0].text.split(" ")[0]
+            season_find = soup.select("li > p.product_article_contents > strong")[1].text
+            season = season_processing(season_word, season_find)
 
-        # 좋아요 개수
-        like = soup.select("li > p > span.prd_like_cnt")[0].text
-        # 조회수
-        view = soup.select("#pageview_1m")[0].text
-        # 누적판매
-        sale = soup.select("#sales_1y_qty")[0].text
-        # 코디 정보
-        coordi = [s.text for s in soup.select("p.style_txt")]
-        coordi = coordi_processing(coordi)
+            # 성별
+            gender = [g.text for g in soup.select("li > p.product_article_contents > span.txt_gender > span")]
 
-        # ~ 18세
-        age18 = soup.select("span.bar_num")[0].text
-        # 19 ~ 23세
-        age19_23 = soup.select("span.bar_num")[1].text
-        # 24 ~ 28세
-        age24_28 = soup.select("span.bar_num")[2].text
-        # 29 ~ 33세
-        age29_33 = soup.select("span.bar_num")[3].text
-        # 34 ~ 39세
-        age34_39 = soup.select("span.bar_num")[4].text
-        # 40 ~
-        age40 = soup.select("span.bar_num")[5].text
-        # 남성 구매 비율
-        man = soup.select("dd.label_info_value")[0].text
-        # 여성 구매 비율
-        woman = soup.select("dd.label_info_value")[1].text
-        # 상품 사진
-        img = 'https:' + soup.select("#detail_bigimg > div.product-img > img")[0]['src']
+            # 좋아요 개수
+            like = soup.select("li > p > span.prd_like_cnt")[0].text
+            # 조회수
+            view = soup.select("#pageview_1m")[0].text
+            # 누적판매
+            sale = soup.select("#sales_1y_qty")[0].text
+            # 코디 정보
+            coordi = [s.text for s in soup.select("p.style_txt")]
+            coordi = coordi_processing(coordi)
 
-        # 옷 아이템 객체
-        clothes = [name, main_category, sub_category, brand, number, tags, price, rating, rating_num, season, gender,
-                   like, view, sale, coordi, age18, age19_23, age24_28, age29_33, age34_39, age40, man, woman, img]
+            # ~ 18세
+            age18 = soup.select("span.bar_num")[0].text
+            # 19 ~ 23세
+            age19_23 = soup.select("span.bar_num")[1].text
+            # 24 ~ 28세
+            age24_28 = soup.select("span.bar_num")[2].text
+            # 29 ~ 33세
+            age29_33 = soup.select("span.bar_num")[3].text
+            # 34 ~ 39세
+            age34_39 = soup.select("span.bar_num")[4].text
+            # 40 ~
+            age40 = soup.select("span.bar_num")[5].text
+            # 남성 구매 비율
+            man = soup.select("dd.label_info_value")[0].text
+            # 여성 구매 비율
+            woman = soup.select("dd.label_info_value")[1].text
+            # 상품 사진
+            img = 'https:' + soup.select("#detail_bigimg > div.product-img > img")[0]['src']
 
-        # 옷 리스트에 아이템 객체 추가
-        clothes_li.append(clothes)
+            # 옷 아이템 객체
+            clothes = [name, main_category, sub_category, brand, number, tags, price, rating, rating_num, season, gender,
+                       like, view, sale, coordi, age18, age19_23, age24_28, age29_33, age34_39, age40, man, woman, img]
 
-        print(f'{num}, : {clothes} |\n{"*" * 20}')
+            # 옷 리스트에 아이템 객체 추가
+            clothes_li.append(clothes)
+
+            print(f'{num}, : {clothes} |\n{"*" * 20}')
+
+        except :
+            pass
 
     driver.quit()
     
