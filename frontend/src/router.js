0979df8914e8939fd7b079bd935451369006c704
kye1115z <kye1115z@naver.com>
2022-07-21 20:09:13 +0900

    feat: Custom, Customtest 파일 추가

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,11 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './Register';
+import Registerrr from './Registerrr';
+import Login from './Login';
+import Search from './Search';
+import Custom from './Custom';
+import Customtest from './Customtest';
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/register' element={<Register />} />
+                <Route path='/register2' element={<Registerrr />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/search' element={<Search />} />
+                <Route path='/custom' element={<Custom />} />
+                <Route path='/customtest' element={<Customtest />} />
+                
             </Routes>
         </BrowserRouter>
     );
